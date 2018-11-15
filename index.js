6bce96547d8cd6b845a5256ddb66f9d54b2da842
Sam Wray <sam@wray.pro>
2018-11-15 10:43:55 +0000

    feat(controls): Adds basic property-control generation

--- index.js
+++ index.js
@@ -13,8 +13,75 @@ import Meyda from 'meyda';
 
 let analyzer;
 
-getUserMedia({ video: false, audio: true })
-  .then((stream) => {
+const simplex = new SimplexNoise(Math.random());
+
+const div = document.createElement('div');
+div.id = 'app';
+
+document.body.appendChild(div);
+
+Vue.component('codemirror', codemirror);
+
+new Vue({
+  el: '#app',
+
+  watch: {
+    code(value) {
+      this.setModule(value);
+    },
+  },
+
+  created() {
+    const { renderers } = this;
+
+    renderers[twoDRenderer.name] = twoDRenderer;
+
+    const context = createContext();
+    const canvas = context.canvas;
+    canvas.style.backgroundColor = '#000';
+
+    document.body.appendChild(canvas);
+
+    let time = 0;
+
+    this.app = createLoop(canvas, {
+      scale: window.devicePixelRatio,
+    });
+
+    this.app.on('tick', (delta) => {
+      const { module, renderers } = this;
+      const { width, height } = context.canvas;
+
+      context.clearRect(0, 0, width, height);
+      const features = analyzer.get(['rms', 'energy']) || {};
+
+      time += delta / 1000;
+
+      const renderContext = {
+        canvas,
+        context,
+        features,
+        delta: time,
+      };
+
+      if (renderers[module.meta.type]) {
+        renderContext.Module = module;
+        renderers[module.meta.type].render(renderContext);
+      }
+    });
+  },
+
+  async mounted() {
+    const { app } = this;
+    let stream;
+
+    try {
+      stream = await getUserMedia({ video: false, audio: true });
+    } catch (e) {
+      console.error(e);
+      return;
+    }
+
     const audioContext = new window.AudioContext({
       latencyHint: 'playback',
     });
@@ -42,66 +109,50 @@ getUserMedia({ video: false, audio: true })
     analyzer.start();
     app.start();
 
-  }).catch((error) => {
-    console.log(error);
-  });
+    this.setModule(this.code);
+  },
 
-const simplex = new SimplexNoise(Math.random());
-const renderers = {};
-const modules = [];
+  methods: {
+    registerModule(module) {
+      const newModule = Object.assign(this.module, module);
+      this.module = Object.assign(this.module.props, newModule);
+      this.props = this.module.props;
+    },
 
-renderers[twoDRenderer.name] = twoDRenderer;
+    setModule(value) {
+      var module = function(str){
+        return eval(`(${str})`);
+      }.call(this, value);
 
-const context = createContext();
-const canvas = context.canvas;
-canvas.style.backgroundColor = '#000';
+      this.registerModule(module);
+    },
+  },
 
-const app = createLoop(canvas, {
-  scale: window.devicePixelRatio
-});
-
-document.body.appendChild(canvas);
-
-let time = 0;
-
-app.on('tick', (delta) => {
-  const [width, height] = app.shape;
-  context.clearRect(0, 0, width, height);
-  const features = analyzer.get(['rms', 'energy']) || {};
-
-  time += delta / 1000;
-
-  const renderContext = {
-    canvas,
-    context,
-    features,
-    delta: time,
-  };
-
-  const modulesLength = modules.length;
-
-  for(let i = 0; i < modulesLength; i += 1) {
-    const module = modules[i];
-    if (renderers[module.meta.type]) {
-      renderContext.Module = module;
-      renderers[module.meta.type].render(renderContext);
-    }
-  }
-});
-
-const div = document.createElement('div');
-div.id = 'app';
-
-document.body.appendChild(div);
-
-Vue.component('codemirror', codemirror);
-
-new Vue({
-  el: '#app',
+  template: `
+    <div>
+      <codemirror v-model="code" :options="cmOptions"></codemirror>
+      <div v-for="(prop, name) in props" style="z-index:1;position:relative;color:#fff">
+        <label v-if="typeof prop === 'number'">{{ name }} <input type="range" v-model.number="module[name]"> {{ module[name] }}</label>
+        <label v-if="typeof prop === 'string'">{{ name }} <input type="text"  v-model.text="module[name]"> {{ module[name] }}</label>
+        <label v-if="typeof prop === 'boolean'" >{{ name }} <input type="checkbox" v-model.boolean="module[name]"> {{ module[name] }}</label>
+      </div>
+    </div>
+  `,
 
   data() {
     return {
-      code: `{
+      renderers: {},
+      module: {},
+      props: {},
+      app: null,
+
+      cmOptions: {
+        mode: 'javascript',
+        lineNumbers: true,
+        extraKeys: { 'Ctrl-Space': 'autocomplete' },
+      },
+
+            code: `{
   meta: {
     type: '2d',
   },
@@ -131,12 +182,14 @@ new Vue({
     return [pointX, pointY];
   },
 
-  numPoints: 10,
-  lineWidth: 0.5,
-  spread: 1,
-  mod: 0,
-  colors: true,
-  animatePoints: false,
+  props: {
+    numPoints: 10,
+    lineWidth: 0.5,
+    spread: 1,
+    mod: 0,
+    colors: true,
+    animatePoints: false,
+  },
 
   draw({ canvas, context, delta, features: { rms, energy } }) {
     const { width, height } = canvas;
@@ -196,36 +249,7 @@ new Vue({
     }
   },
 }`,
-      cmOptions: {
-        mode: 'javascript',
-        lineNumbers: true,
-        extraKeys: { 'Ctrl-Space': 'autocomplete' },
-      },
     };
   },
-
-  watch: {
-    code(value) {
-      this.setModule(value);
-    },
-  },
-
-  mounted() {
-    this.setModule(this.code);
-  },
-
-  methods: {
-    setModule(value) {
-      var module = function(str){
-        return eval("(" + str + ")");
-      }.call(this, value);
-
-      modules[0] = module;
-    },
-  },
-
-  template: `
-    <codemirror v-model="code" :options="cmOptions"></codemirror>
-  `,
 });
 
